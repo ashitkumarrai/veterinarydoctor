77f5bf328d8874f2b16000b546584f06da80ad40
Ashit Kumar Rai <68168801+ashitkumarrai@users.noreply.github.com>
2022-09-26 13:23:59 +0530

    jwt

--- src/test/java/com/appointment/veterinarydoctor/VeterinarydoctorApplicationTests.java
+++ src/test/java/com/appointment/veterinarydoctor/VeterinarydoctorApplicationTests.java
@@ -1,13 +1,30 @@
 package com.appointment.veterinarydoctor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
+@DataJpaTest
 class VeterinarydoctorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
+	void createToken() 
+		{
+			RestTemplate template = new RestTemplate();
+			Book b=new Book();
+			b.setName("ammullu");
+			b.setId(20);
 
+		    ResponseEntity<String> result = template.postForEntity("http://localhost:8080/Book/",b, String.class);
+		     
+		    //Verify request succeed				
+		    assertEquals(201,result.getStatusCodeValue());
+		    System.out.println("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"+result.getStatusCodeValue());
+		    }
+			
 }
